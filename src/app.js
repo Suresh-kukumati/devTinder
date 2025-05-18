2fae81c86dbfa3f690787e4678b562a5dd9568fe
PratibhaMS <pratibhamsawant0613@gmail.com>
2025-05-18 15:16:00 +0530

    Multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -6,10 +6,36 @@ const app = express();
 //   res.send("Hi i am server with new");
 // });
 
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send("Hi i am server with new");
-});
+app.get(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Handling first user");
+      // res.send("I am the response of first user");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling second user");
+      // res.send("I am the response of Second user");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling Third user");
+    // res.send("I am the response of Third user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling Fourth user");
+    // res.send("I am the response of Fourth user");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling Fifth user");
+    res.send("I am the response of Fifth user");
+    // next();
+  }
+);
 
 app.listen(3000, () => {
   console.log("server is listening");
